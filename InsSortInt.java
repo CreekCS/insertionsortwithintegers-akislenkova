bb0f49de0acfcd24a31e97140a68fde5d19f089b
Anna Kislenkova <anna.kislenkova@icloud.com>
2024-08-30 06:04:53 +0000

    submission

--- InsSortInt.java
+++ InsSortInt.java
@@ -3,34 +3,40 @@ import java.util.ArrayList;
 import java.io.File;
 import java.io.FileNotFoundException;
 public class InsSortInt {
-   public static void main(String[] args){
-        try{
-            File myFile = new File("randInts.txt");
-            Scanner reader = new Scanner(myFile);
-            ArrayList<String> ints = new ArrayList<String>(1000);
-                while(reader.hasNextLine()){
-                    ints.add(reader.nextLine());
-                }
-                reader.close();
-                int n = ints.size();
-        }
-        catch(FileNotFoundException e){
-            ArrayList<String> ints = new ArrayList<String>(1000);
-            int n = ints.size();
-            int[] sortedInts = new int[n];
-            for(int i=0; i<n; i++){
-                int key = Integer.parseInt(ints.get(i));
-                int j = i-1;
-                while((j>=0) && (Integer.parseInt(ints.get(j))>key)){
-                        sortedInts[j+1] = Integer.parseInt(ints.get(j));
-                        j--;
-                        sortedInts[j+1] = key;
+
+    public static ArrayList<Integer> sortInsertion(ArrayList<Integer> a) {
+        ArrayList<Integer> sorted = new ArrayList<Integer>(a.size());
+        for(int i=0; i<a.size(); i++){
+            Integer current = a.get(i);
+            
+            int k=0;
+            for (; k<i; k++) {
+                if (current < sorted.get(k)) {
+                    break;
                 }
             }
-        System.out.println("Smallest: " + sortedInts[0]);
-        System.out.println("Largest: " + sortedInts[999]);
+            sorted.add(k, current);
         }
-        
+        return sorted;
+
+    }
+    public static void main(String[] args){
+        try {
+            File myFile = new File("randInts.txt");
+            Scanner reader = new Scanner(myFile);
+            ArrayList<Integer> ints = new ArrayList<Integer>(1000);
+            while(reader.hasNextLine()){
+                ints.add(Integer.parseInt(reader.nextLine()));
+            }
+            reader.close();
+            ArrayList<Integer> sorted = sortInsertion(ints);
+
+           System.out.println("Smallest: " + sorted.get(0));
+           System.out.println("Largest: " + sorted.get(sorted.size()-1));
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("File not found.");
         }
         
     }
+}
